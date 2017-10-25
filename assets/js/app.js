f1036191404477b88535be0329ed65ef48d18283
Nerajno <nerajno@gmail.com>
2017-10-25 18:28:42 -0400

    structure and little js

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+//Runs Jquery
+$(document).ready(function() {
+  // Fetches FCC Strema info and api status call
+  var url ="https://wind-bow.gomix.me/twitch-api";// Fcc twitch
+  console.log(url);
+  $.getJSON(url,function(data1){
+
+  });
+});
