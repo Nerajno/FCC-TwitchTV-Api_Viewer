7d0328995e58581ec127674830b8c012f1934c6d
Nerajno <nerajno@gmail.com>
2017-10-30 07:50:53 -0400

    connected the api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 //Runs Jquery
 $(document).ready(function() {
-  // Fetches FCC Strema info and api status call
-  var url ="https://wind-bow.gomix.me/twitch-api";// Fcc twitch
+  // Fetches FCC Stream info and api status call
+  var url ="https://api.twitch.tv/kraken/users/RealKraftyy?client_id=m4cp44uncfyahuh98kosi5f0j97bz0";// Fcc twitch
   console.log(url);
   $.getJSON(url,function(data1){
 
   });
 });
+// lestream
