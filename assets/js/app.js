60676a688725f4c501067cec79e67d082ba38461
Nerajno <nerajno@gmail.com>
2017-11-19 00:47:51 -0500

    Logged all the streams to see through an iteration.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,21 @@
+var channels =["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404","RealKraftyy?"]
 //Runs Jquery
-$(document).ready(function() {
+$(document).ready(function(){
   // Fetches FCC Stream info and api status call
-  var url ="https://api.twitch.tv/kraken/users/RealKraftyy?client_id=m4cp44uncfyahuh98kosi5f0j97bz0";// Fcc twitch
-  console.log(url);
-  $.getJSON(url,function(data1){
-
+  //Test
+  // var url ="https://wind-bow.glitch.me/twitch-api/streams/"+channels[10]
+  //The main api
+  var url2 = "https://wind-bow.glitch.me/twitch-api/streams/"
+  var i = 0;
+  var channelStatus = data1.stream;
+  while (i <channels.length){
+    console.log(url2+channels[i]+channelStatus);
+    i++;
+  }
+  $.getJSON(url2,function(data1){
+    // var channelStatus = data1.stream;
+    // console.log(channelStatus);
   });
 });
+
 // lestream
