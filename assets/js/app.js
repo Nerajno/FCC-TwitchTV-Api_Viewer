268902a6641f51d76e90504d44ab1af763773391
Nerajno <nerajno@gmail.com>
2017-11-23 17:35:53 -0500

    got some more tweaking done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var channels =["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404","RealKraftyy?"]
+var channels =["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404","RealKraftyy?"];
 //Runs Jquery
 $(document).ready(function(){
   // Fetches FCC Stream info and api status call
@@ -6,15 +6,18 @@ $(document).ready(function(){
   var url2 = "https://wind-bow.glitch.me/twitch-api/streams/";
   var i = 0;
   while (i <channels.length){
-    console.log(url2+channels[i]);
+    var url3 = url2+channels[i];
+    // console.log(url3);
+    $.getJSON(url3,function(data){
+      console.log(url3);
+      var exercise = data.stream;
+      console.log(exercise);
+      if (exercise === null) {
+        console.log("not streaming");
+      } else {
+        console.log("streaming");
+      }
+    });
     i++;
   }
-  var testing = url2+channels[1]
-  var exercise = url2+channels[3]
-  // console.log(testing);
-  $.getJSON(exercise,function(data){
-    var exercise = data.stream;
-    console.log(exercise);
-
   });
-});
