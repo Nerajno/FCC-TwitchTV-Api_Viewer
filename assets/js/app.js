7fd93de2b006c2f4e6c1daccf9e7aa2811c72018
Nerajno <nerajno@gmail.com>
2017-12-15 21:50:35 -0500

    Made it log online vs offline

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,29 @@
-var channels =["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", 
-               "RobotCaleb", "noobs2ninjas", "brunofin", "comster404","RealKraftyy?"];
-//Runs Jquery
-$(document).ready(function(){
-  // Fetches FCC Stream info and api status call
-  //The main api
-  var url2 = "https://wind-bow.glitch.me/twitch-api/streams/";
-  var i = 0;
-  while (i <channels.length){
-    var url3 = url2+channels[i];
-    // console.log(url3);
-    $.getJSON(url3,function(data){
-      console.log(url3);
-      var exercise = data.stream;
-      console.log(exercise);
-      if (exercise === null) {
-        console.log("not streaming");
+
+
+var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx",
+    "RobotCaleb", "noobs2ninjas", "brunofin", "comster404", "RealKraftyy?"];
+
+var apiSegmentOne = "https://wind-bow.glitch.me/twitch-api/streams/";
+
+//This helped me to run the function channel by channel.
+// //https://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
+function individualIteration(i) {
+         return function(){
+           console.log(channels[i]);
+        }
+         }
+$(document).ready(function () {
+    for (var i = 0; i < channels.length; i++) {
+      var apiSegmentTwo = apiSegmentOne+channels[i];
+       $.getJSON(apiSegmentTwo, function (data) {
+         var intitalData = data.stream;
+         var test = data.links;
+      if (intitalData === null) {
+        console.log("The channel is offline.");
       } else {
-        console.log("streaming");
+         var user = data.stream.channel.display_name;
+        console.log(user+" is online.");
       }
-    });
-    i++;
-  }
+       });
+    }
   });
