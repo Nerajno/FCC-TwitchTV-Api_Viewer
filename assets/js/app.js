8b6cb03fc662ade045c261a36485366df1e90bcc
Nerajno <nerajno@gmail.com>
2017-12-29 23:41:43 -0500

    testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+// $('#myCollapsible').collapse({
+//   toggle: false
+// })
+
 var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx",
     "RobotCaleb", "noobs2ninjas", "brunofin", "comster404", "RealKraftyy?"];
 
@@ -11,8 +15,8 @@ function individualIteration(i) {
         }
          }
 $(document).ready(function (){
-      console.log(channels);
-      console.log(channels.length);
+      // console.log(channels);
+      console.log(channels.length, channels);
     for (var i = 0; i < channels.length; i++) {
       var apiSegmentTwo = apiSegmentOne+channels[i];
        $.getJSON(apiSegmentTwo, function (data) {
@@ -24,7 +28,17 @@ $(document).ready(function (){
          console.log(conversion+" is offline.");
       } else {
          var user = data.stream.channel.display_name;
+         var info = data.stream;
+         var gameName = data.stream.channel.game;
+         let status = data.stream.channel.status;
+         let viewers = data.stream.viewers;
+         let logo = data.stream.channel.logo;
+         let channelAddress = data.stream.channel.url;
         console.log(user+" is online.");
+        console.log("Each Info", info, gameName, status, viewers, logo,
+        channelAddress);
+
+
       }
        });
     }
